873493c7b6baffcd68f07f23842c52837c11ebcc
Mohammad Rahmani <rto1680@gmail.com>
2019-10-03 14:19:20 +0430

    Initail commit

---
+++ android/app/src/main/kotlin/com/example/day7_button_animation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.day7_button_animation
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
